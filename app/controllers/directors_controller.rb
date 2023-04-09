152f4eb4a2907df97586b622f56b3d706068392d
Jorge Gomez <86806874+Gomezzzer@users.noreply.github.com>
2023-04-09 16:49:36 +0000

    working on youngest director

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -7,8 +7,14 @@ class DirectorsController < ApplicationController
   end
 
   def wisest
+    @oldest = Director.where.not({ :dob => nil }).order({ :dob => :asc }).at(0)
 
     render({ :template => "director_templates/eldest.hmtl.erb" })
   end
+
+ def youngest
+
+  render({ :template => "director_templates/youngest.html.erb" })
+ end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  get("directors/eldest", { :controller => "directors", :action => "wisest" })
 
   get("/", { :controller => "application", :action => "homepage" })
 
   get("/directors", { :controller => "directors", :action => "index" })
+
+  get("directors/youngest", { :controller => "directors", :action => "youngest" })
+
+  get("directors/eldest", { :controller => "directors", :action => "wisest" })
+
+ 
+
+  
 end
